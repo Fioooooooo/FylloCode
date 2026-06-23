433e2db60e89ebb3bb4f2a60eb94571a50910675
Fio <fuyitao2018@gmail.com>
2026-06-23 15:03:04 +0800

    deps(nuxt): upgrade nuxt ui to 4.9.0 for override .nuxt-ui directory location

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -94,6 +94,7 @@ export default defineConfig(({ command }) => ({
       }),
       vue(),
       ui({
+        root: __dirname,
         autoImport: {
           eslintrc: {
             enabled: true,
